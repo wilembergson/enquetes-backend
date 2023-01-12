7490513099e0d036e16712d66291117c3c700a74
wilembergson <wilembergson@yahoo.com>
2023-01-12 00:00:56 -0300

    chore: starting

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+export default {
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  roots: [
+    "<rootDir>/src"
+  ],
+  testEnvironment: "node",
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+};
