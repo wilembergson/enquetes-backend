45b17d146f66d12f6b0448b9928dbd6e7c60abf9
wilembergson <wilembergson@yahoo.com>
2023-02-01 18:11:47 -0300

    refactor: rename TokenGenerator to Encrypter

--- src/data/use-cases/add-account/db-add-account-protocols.ts
+++ src/data/use-cases/add-account/db-add-account-protocols.ts
@@ -1,4 +1,4 @@
 export * from "../../../domain/model/account";
 export * from "../../../domain/use-cases/add-account";
-export * from "../../protocols/cryptografy/encrypter";
+export * from "../../protocols/cryptografy/hasher";
 export * from "../../protocols/db/add-account-repository";
--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -16,20 +16,13 @@ describe('Bcrypt Adapter', () => {
   it('should call bcrypt with correct value', async () => {
     const sut = makeSut()
     const hashSpy = jest.spyOn(bcrypt, 'hash')
-    await sut.encrypt('any_value')
+    await sut.hash('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
   })
 
   it('should return a hash on sucess', async () => {
     const sut = makeSut()
-    const hash = await sut.encrypt('any_value')
+    const hash = await sut.hash('any_value')
     expect(hash).toBe('hash')
   })
-
-  /*it('should throw if bcrypt throws', async () => {
-    const sut = makeSut()
-    jest.spyOn(bcrypt, 'hash').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
-    const promise = sut.encrypt('any_value')
-    await expect(promise).rejects.toThrow()
-  })*/
 })
